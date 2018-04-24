ee4537202b7860e3f48848331dfdbf38b825b4e9
elvis <theelvina@gmail.com>
2018-04-24 16:42:09 +0100

    homepage updates

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -8,26 +8,28 @@ if($slogan):
  ?>
 <div class="uk-grid-match hero-grid uk-child-width-1-2@s uk-grid-collapse" uk-grid>
     <div class="uk-width-1-1 clearboxside">
-        <div class="uk-card uk-card-default "><h1 class="uk-heading-hero uk-text-uppercase uk-text-primary"><?php echo $slogan; ?></h1></div>
+        <div class="uk-card uk-card-default "><?php the_post_thumbnail("full");?>
+        </div>
     </div>
-
-
-
 <?php 
 endif;
 ?>
-
-
     <div class="">
-        <div class="uk-padding"><div class=""><?php the_content(); ?></div></div>
+        <div class="uk-padding-small uk-margin-top-m "><div class=""><h1 class="uk-heading-hero uk-text-uppercase uk-text-primary  uk-text-center uk-text-left@s"><?php echo $slogan; ?></h1> <?php the_content(); ?></div></div>
     </div>
 
-    <div class="uk-padding">
+    <div class="">
+      
         <div class="uk-grid split" uk-grid>
-            <div class="uk-padding uk-padding-remove-top"><h3 class="uk-text-uppercase"> <?php echo $text; ?> </h3></div>
-            <div class=""><h3 class="uk-text-uppercase uk-text-primary"> <?php echo $text3; ?> </h3></div>
+            <div class="">
+                <div class="uk-padding-small uk-margin-top-m uk-padding-remove-top"><h3 class="uk-text-uppercase"> <?php echo $text; ?> </h3></div>
+            </div>
+            <div>
+                <div class="uk-padding-small uk-margin-remove-left uk-margin-remove-top "><h3 class="uk-text-uppercase uk-text-primary"> <?php echo $text3; ?> </h3></div>
+            </div>
         
         </div>
+
     </div>
 
 </div>
